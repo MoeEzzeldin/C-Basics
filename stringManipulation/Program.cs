4badcc6ae4f6ff38d0abb20bdc567396101bb071
Student <ahmed.fakher.e@gmail.com>
2024-01-15 21:58:23 -0500

    learning new concepts around strings

---
+++ stringManipulation/Program.cs
@@ -0,0 +1,19 @@
+﻿// learning how to creat a list of <T>. add new and sort.
+// order from the end of the list concept / loop for each name in names "list".
+
+var names = new List<string>
+ {
+     "Moe",
+     "Abby",
+     "Alia",
+ };
+    names.Add("Adam");
+    names.Add("Sayed");
+    names.Add("Hassan");
+    names.Sort();
+
+    Console.WriteLine(names[^1]);
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}");
+};
